b320efe7dd9327da3c2d137c99fb82210a58011f
Boris Radulov <sc20br@leeds.ac.uk>
2021-06-06 22:04:41 +0100

    Finished binary.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var fs = require('fs');
+#!/usr/bin/env node
+
+const fs = require('fs');
+const yargs = require("yargs");
 
 const words = fs.readFileSync('wordlist.txt', 'ascii').split('\n');
 
@@ -13,16 +16,14 @@ function encodeIP(ip) {
     let wordcombo = "";
     for(let i = 0; i < 4; i++) {
         wordcombo += words[numbers[i]];
-        if(i != 3) {
-            wordcombo += ' ';
-        }
+        if(i != 3) wordcombo += ' ';
     }
 
     return wordcombo;
 }
 
 function decodeIP(wordcombo) {
-    const regex = new RegExp('^([A-z]{2,}[ ]){3}[A-z]{2,}');
+    const regex = new RegExp('^([A-z]{2,}[ ]){3}[A-z]{2,}$');
     if(!regex.test(wordcombo)) {
         throw new Error("Invalid word combination passed to decodeIP()");
     }
@@ -30,13 +31,30 @@ function decodeIP(wordcombo) {
     const decodedWords = wordcombo.split(' ');
 
     let ip = "";
-
     for(let i = 0; i < 4; i++) {
         ip += words.indexOf(decodedWords[i]);
-        if(i != 3) {
-            ip += '.';
-        }
+        if(i != 3) ip += '.';
     }
 
     return ip;
+}
+
+exports.words = words;
+exports.encodeIP = encodeIP;
+exports.decodeIP = decodeIP;
+
+const options = yargs
+    .usage('-d "words" | -e ip')
+    .option('d', {alias : 'decode', describe : 'String to be decoded', type : 'string', demandOption : false})
+    .option('e', {alias : 'encode', describe : 'IP to be encoded', type: 'string', demandOption : false})
+    .argv;
+
+try {
+    if(options.decode)
+        console.log(decodeIP(options.decode));
+    if(options.encode)
+        console.log(encodeIP(options.encode));
+} catch (error) {
+    console.log(error.message);
+    process.exit(-1);
 }
